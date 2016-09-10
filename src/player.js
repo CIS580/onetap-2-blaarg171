57fdc5af077320f3f7c9940a886704369c634930
Lowell Scott <blaarg171@gmail.com>
2016-09-09 21:42:54 -0500

    Stuff from class

--- src/player.js
+++ src/player.js
@@ -13,24 +13,36 @@ module.exports = exports = Player;
 function Player(position) {
   this.x = position.x;
   this.y = position.y;
-  this.width  = 16;
+  this.width = 16;
   this.height = 16;
-  this.spritesheet  = new Image();
+  this.spritesheet = new Image();
   this.spritesheet.src = encodeURI('assets/link/not link/notlink up.png');
+
+  var self = this;
+  window.onmouseclick = function (event) {
+    self.x = event.clientX;
+    self.state = "walking";
+  }
 }
 
 /**
  * @function updates the player object
  * {DOMHighResTimeStamp} time the elapsed time since the last frame
  */
-Player.prototype.update = function(time) {}
+Player.prototype.update = function (time) {
+  switch (this.state) {
+    case "walking":
+      this.y -= 1;
+      break;
+  }
+}
 
 /**
  * @function renders the player into the provided context
  * {DOMHighResTimeStamp} time the elapsed time since the last frame
  * {CanvasRenderingContext2D} ctx the context to render into
  */
-Player.prototype.render = function(time, ctx) {
+Player.prototype.render = function (time, ctx) {
   ctx.drawImage(
     // image
     this.spritesheet,
