5e5de89aa3d0722451062cb84ad6c9d42ea42954
blaarg171 <blaarg171@users.noreply.github.com>
2016-09-12 15:16:51 -0500

    stuff from class

---
+++ eyebat.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = exports = Eyebat;
+
+function Eyebat(Postition) {
+	
+}
+
+eyebat.prototype.update = function(time) {
+	
+}
+
+eyebat.prototype.render = function(time, ctx) {
+	
+}
